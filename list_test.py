ba44e5f7a73d9024ba95355573e183ea60630065
mashilu <mslbuaa@163.com>
2018-03-14 17:41:53 +0800

    modify list_test.py

--- list_test.py
+++ list_test.py
@@ -22,3 +22,24 @@ print(poppedmotor)
 
 motorcycles.remove('honda')
 print(motorcycles)
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+print(players[2:])
+
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print(my_foods)
+print(friend_foods)
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print(my_foods)
+print(friend_foods)
+
+list_len = len(players)
+start = (list_len - 3) // 2
+print(players[start:start+3])
+
