33c62d45a6dcf2f1449008f150d8575e77973154
mashilu <mslbuaa@163.com>
2018-03-29 23:36:52 +0800

    ****

---
+++ pytest/dic_test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+alien_0 = dict()
+alien_0['color'] = 'green'
+alien_0['point'] = 5
+
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
+
+print("The alien is " + alien_0['color'])
+alien_0['color'] = 'yellow'
+print("The alien is " + alien_0['color'])
