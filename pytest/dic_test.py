005c97200efb15d312e62771897e4b062dd37e60
mashilu <mslbuaa@163.com>
2018-04-08 22:56:30 +0800

    ******

--- pytest/dic_test.py
+++ pytest/dic_test.py
@@ -14,3 +14,30 @@ print(alien_0)
 print("The alien is " + alien_0['color'])
 alien_0['color'] = 'yellow'
 print("The alien is " + alien_0['color'])
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("x-position: " + str(alien_0['x_position']))
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position: " + str(alien_0['x_position']))
+
+print(alien_0)
+del alien_0['speed']
+print(alien_0)
+
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+
+for k, v in user_0.items():
+    print("\nKEY: " + k)
+    print("\nVALUE: " + v)
---
+++ pytest/while_test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program:"
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     if message != 'quit':
+#         print(message)
+
+active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+prompt = "\nPlease enter the name of a city you have visited: "
+prompt += "\n(Enter 'quit' when you are finished。）"
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to go to " + city.title() + "!")
