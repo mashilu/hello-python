58eb1d68326296bb9f665f9eabb09f71089baab0
mashilu <mslbuaa@163.com>
2018-04-10 09:56:03 +0800

    ****

--- pytest/while_test.py
+++ pytest/while_test.py
@@ -8,21 +8,47 @@ prompt += "\nEnter 'quit' to end the program:"
 #     if message != 'quit':
 #         print(message)
 
-active = True
-while active:
-    message = input(prompt)
-    if message == 'quit':
-        active = False
-    else:
-        print(message)
+# active = True
+# while active:
+#     message = input(prompt)
+#     if message == 'quit':
+#         active = False
+#     else:
+#         print(message)
+#
+# prompt = "\nPlease enter the name of a city you have visited: "
+# prompt += "\n(Enter 'quit' when you are finished。）"
 
-prompt = "\nPlease enter the name of a city you have visited: "
-prompt += "\n(Enter 'quit' when you are finished。）"
+# while True:
+#     city = input(prompt)
+#
+#     if city == 'quit':
+#         break
+#     else:
+#         print("I'd love to go to " + city.title() + "!")
 
-while True:
-    city = input(prompt)
+# responses = {}
+# while True:
+#     repeat = input("\nWould you like to respond (yes/no) ?")
+#     if repeat == 'no':
+#         break
+#     name = input("\nWhat is your name?")
+#     response = input("\nWhich mountain would you like to climb someday? ")
+#     responses[name] = response
+#
+# print("\n--- Poll Results ---")
+# for name, response in responses.items():
+#     print(name + " would like to climb " + response)
 
-    if city == 'quit':
-        break
-    else:
-        print("I'd love to go to " + city.title() + "!")
+sandwich_orders = ["sandwicha", "sandwichb", "sandwichc"]
+finished_sandwiches = []
+
+while sandwich_orders:
+    curr_order = sandwich_orders.pop()
+    print("I made your tuna sandwich" + curr_order)
+    finished_sandwiches.append(curr_order)
+
+print("finished sandwicher are:")
+
+for sandwich in finished_sandwiches:
+    print("\t" + sandwich)
